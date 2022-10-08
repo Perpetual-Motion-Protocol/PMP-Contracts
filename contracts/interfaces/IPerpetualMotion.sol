2072e1e159fd79df84abbc6acdf7a179bcf372a6
Christopher Dancy <chris.dancy@decentlabs.io>
2022-10-08 12:35:11 -0500

    added events to interface

--- contracts/interfaces/IPerpetualMotion.sol
+++ contracts/interfaces/IPerpetualMotion.sol
@@ -33,4 +33,23 @@ interface IPerpetualMotion {
         string description,
         uint256 fundingGoal
     );
+
+    event ContributionUpdated(
+        uint256 indexed projectId,
+        uint256 contribution,
+        address indexed contributor,
+        address token
+    );
+
+    event Executed(
+        uint256[] projectIds,
+        address[][] contributers,
+        bytes[][] roundUpDatas
+    );
+
+    event PledgeCreated(
+        uint256 projectId,
+        Strategies strategy,
+        bytes strategyData
+    );
 }
