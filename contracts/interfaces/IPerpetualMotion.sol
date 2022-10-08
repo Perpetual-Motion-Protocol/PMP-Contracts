0b570e5af8c568f4ed53a13522404fa90940a84d
Christopher Dancy <chris.dancy@decentlabs.io>
2022-10-08 00:31:24 -0500

    updated interface

--- contracts/interfaces/IPerpetualMotion.sol
+++ contracts/interfaces/IPerpetualMotion.sol
@@ -7,12 +7,21 @@ interface IPerpetualMotion {
         string description;
         address fundingAddress;
         uint256 fundingGoal;
+        uint256 amountFunded;
         uint256 startTime;
         uint256 endTime;
         address[] contributorAddresses;
     }
 
+    struct ContributerStrategy {
+        Strategies strategyType;
+        bytes strategyData; // Freq - Amount - blah, blah, blah
+        uint256 prevDonation;
+        uint256 totalDonated;
+    }
+
     enum Strategies {
+        NoStrategy,
         PeriodicLumpSum,
         Stream,
         Roundup
